8cc34e0adc52d359c3aba0c3e83fbeed2e6541d2
Gutu Galuppo <galuppodev@gmail.com>
2020-11-26 18:37:43 +0100

    delete unused stuff

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Contact from './Contact';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Contact />
   </React.StrictMode>,
   document.getElementById('root')
 );
